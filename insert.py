ee320bc52fb674daaeccefe3b5b0976c8d76664d
YanpuLi <bonnieli1025@gmail.com>
2015-10-26 21:38:23 -0400

    update

--- insert.py
+++ insert.py
@@ -6,31 +6,34 @@ The function of this file is to parse csv file into json format(array of documen
 insert.py:
 This file is for inserting the documents into MongoDB
 
-Here, we use "test" as the db
-
-"biometrics" as the collection name
+EXAMPLE:
+python insert.py --db ** --col ** --path **
 
 """
 
 from fileProcess import process_file
+from pymongo import MongoClient
+import glob
+import argparse
 
+parser = argparse.ArgumentParser(description='data uploading')
+parser.add_argument('--db', action='store', dest='dbName', help='store the db name')
+parser.add_argument('--col', action='store',dest='dbCollection',help='store the collection name')
+parser.add_argument('--path', action='store',dest='filePath',help='store the file path')
+result = parser.parse_args()
+# connecting to DB
+client = MongoClient("mongodb://localhost:27017")
+db = client[result.dbName]
 
-def insert_autos(infile, db):
+def insert_autos(infile, db, col):
     data = process_file(infile)
-    #Inserting into collection "biometrics"
-    db.biometrics.insert(data)
-    print "import:%i pieces of documents into MongoDB"%(len(data)) 
-  
-if __name__ == "__main__":
-    # Code here is for local use on your own computer.
-    from pymongo import MongoClient
-    import glob
-
-    # connecting to DB
-    client = MongoClient("mongodb://localhost:27017")
-    db = client.test
-
+    db[col].insert(data)
+    print "import:%i pieces of documents into MongoDB"%(len(data))
     # reading in all the csv files
-    for files in glob.glob("/Users/Lesley/Desktop/bio/*.csv"):
-        insert_autos(files, db)
-        print db.biometrics.find_one()
+for files in glob.glob("%s/*.csv"%result.filePath):
+    insert_autos(files, db,result.dbCollection)
+    print db[result.dbCollection].find_one()
+
+  
+
+
